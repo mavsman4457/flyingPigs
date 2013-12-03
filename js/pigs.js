095d467991e66bc4554c8eee974859fd0e0b6b39
Brady Dowling <bdsport445@yahoo.com>
2013-12-02 21:58:25 -0500

    initial push of code, doesn't show pigs from c9.io preview but it shows it on my machine

---
+++ js/pigs.js
@@ -0,0 +1,94 @@
+var pigs = new Array();
+var showing = new Array(0, 0, 0, 0, 0, 0);
+
+$(document).ready(function(){
+    $(document).keyup(function(e){
+        var key = e.which;
+        var actualKey = null;
+        //sets the actual key to what is shown on the keyboard
+        switch(key)
+        {
+            case 48:
+                actualKey = 0;
+                break;
+            case 49:
+                actualKey = 1;
+                break;
+            case 50:
+                actualKey = 2;
+                break;
+            case 51:
+                actualKey = 3;
+                break;
+            case 52:
+                actualKey = 4;
+                break;
+            case 53:
+                actualKey = 5;
+                break;
+            case 54:
+                actualKey = 6;
+                break;
+            /*case 55:
+                actualKey = 7;
+                break;
+            case 56:
+                actualKey = 8;
+                break;
+            case 57:
+                actualKey = 9;
+                break;*/
+        }
+        var pigNum = actualKey-1;
+        var topCoord = pigs[pigNum].offset().top;
+        if(showing[pigNum] == 1){
+            pigs[pigNum].offset({top:topCoord, left:-600});
+            showing[pigNum] = 0;
+            playSound('sounds/oink.mp3');
+            showing[pigNum] = 2;                    
+        }
+    });
+});
+
+function playSound(soundfile) {
+    document.getElementById("dummy").innerHTML=
+    "<embed src=\""+soundfile+"\" hidden=\"true\" autostart=\"true\" loop=\"false\" />";
+}
+
+
+$(document).ready(function(){
+    pigs[0] = $('#one');
+    pigs[1] = $('#two');
+    pigs[2] = $('#three');
+    pigs[3] = $('#four');
+    pigs[4] = $('#five');
+    pigs[5] = $('#six');
+    for(var i = 0; i < pigs.length; i++)
+    {
+        scrollPig(i);
+    }
+});
+
+
+function scrollPig(pigNum){
+    setInterval(function(){
+        //var pig = $('#one');
+        var topCoord = pigs[pigNum].offset().top;
+        //console.log(pigs[pigNum].offset().left);
+        if(pigs[pigNum].offset().left > 600){
+            rollBack(pigNum);
+        } else if(pigs[pigNum].offset().left < 600 && pigs[pigNum].offset().left > -40) {
+            showing[pigNum] = 1;
+        }
+        var topper = pigs[pigNum].offset().top;
+        pigs[pigNum].animate({
+            left : "+=10"
+        },{ duration: 200, queue: false });
+    },200);
+}
+
+function rollBack(pigNum){
+    var topCoord = pigs[pigNum].offset().top;
+    pigs[pigNum].offset({top:topCoord, left:-600});
+    showing[pigNum] = 0;
+}
